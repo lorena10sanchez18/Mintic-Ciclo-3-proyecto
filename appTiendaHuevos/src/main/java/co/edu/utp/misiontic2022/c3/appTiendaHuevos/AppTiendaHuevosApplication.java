f94cf5cf616450935874267d3c018056b9bac827
Jonathan Stiven Pulido Ramirez <idiqe2@gmail.com>
2022-09-07 23:33:26 -0500

    Se descomprime el proyecto que esta en spring boot

---
+++ appTiendaHuevos/src/main/java/co/edu/utp/misiontic2022/c3/appTiendaHuevos/AppTiendaHuevosApplication.java
@@ -0,0 +1,13 @@
+package co.edu.utp.misiontic2022.c3.appTiendaHuevos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppTiendaHuevosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppTiendaHuevosApplication.class, args);
+	}
+
+}
---
+++ appTiendaHuevos/src/test/java/co/edu/utp/misiontic2022/c3/appTiendaHuevos/AppTiendaHuevosApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.utp.misiontic2022.c3.appTiendaHuevos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTiendaHuevosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
