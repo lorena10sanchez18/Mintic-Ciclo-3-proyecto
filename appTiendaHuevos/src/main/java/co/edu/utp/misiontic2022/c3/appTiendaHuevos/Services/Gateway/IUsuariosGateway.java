432aa9c16758d2e0569904ff620d540c05acf3bd
Jonathan Stiven Pulido Ramirez <idiqe2@gmail.com>
2022-09-08 23:10:40 -0500

    Se crea un contrato que implementa el services del Usuario con el fin de separar la logica del repository y el services

--- appTiendaHuevos/src/main/java/co/edu/utp/misiontic2022/c3/appTiendaHuevos/Services/Gateway/IUsuariosGateway.java
+++ appTiendaHuevos/src/main/java/co/edu/utp/misiontic2022/c3/appTiendaHuevos/Services/Gateway/IUsuariosGateway.java
@@ -1,2 +1,8 @@
-package co.edu.utp.misiontic2022.c3.appTiendaHuevos.Services.Gateway;public interface IUsuariosGateway {
+package co.edu.utp.misiontic2022.c3.appTiendaHuevos.Services.Gateway;
+
+import co.edu.utp.misiontic2022.c3.appTiendaHuevos.Model.Usuarios;
+
+public interface IUsuariosGateway {
+
+    Usuarios buscarUsuarioPorCedula(Integer cedula);
 }
