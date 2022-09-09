8a9559340922316a11a8b99373417c924d5698ea
Jonathan Stiven Pulido Ramirez <idiqe2@gmail.com>
2022-09-08 23:08:29 -0500

    Se crea una intefaz de Usuarios que contiene el repositorio de JPA el cual nos ayuda a usazr sus metodos para interactuar con la base de datos, se crea una busqueda por cedula que entrega un objeto de tipo Usuarios

--- appTiendaHuevos/src/main/java/co/edu/utp/misiontic2022/c3/appTiendaHuevos/Repository/IUsuariosRepository.java
+++ appTiendaHuevos/src/main/java/co/edu/utp/misiontic2022/c3/appTiendaHuevos/Repository/IUsuariosRepository.java
@@ -1,9 +1,11 @@
 package co.edu.utp.misiontic2022.c3.appTiendaHuevos.Repository;
 
-import co.edu.utp.misiontic2022.c3.appTiendaHuevos.Model.Usuario;
+import co.edu.utp.misiontic2022.c3.appTiendaHuevos.Model.Usuarios;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IUsuarioRepository extends JpaRepository<Usuario, Integer> {
+public interface IUsuariosRepository extends JpaRepository<Usuarios, Integer> {
+
+    Usuarios findByCedula(Integer cedula);
 }
