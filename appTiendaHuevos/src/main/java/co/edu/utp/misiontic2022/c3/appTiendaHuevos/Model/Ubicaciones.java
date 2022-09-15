b40cbc87810ea29e667d937e2b2ec73f9bd7514d
Pedro Gustavo Melendez Rivera <pmelendez@sena.edu.co>
2022-09-14 20:30:23 -0500

    se creo la representacion de la tabla ubicaciones de la base de datos

--- appTiendaHuevos/src/main/java/co/edu/utp/misiontic2022/c3/appTiendaHuevos/Model/Ubicaciones.java
+++ appTiendaHuevos/src/main/java/co/edu/utp/misiontic2022/c3/appTiendaHuevos/Model/Ubicaciones.java
@@ -1,2 +1,26 @@
-package co.edu.utp.misiontic2022.c3.appTiendaHuevos.Model;public class Ubicaciones {
+package co.edu.utp.misiontic2022.c3.appTiendaHuevos.Model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "ubicaciones")
+public class Ubicaciones {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_ubicacion",  nullable = false, unique = true)
+    private Integer idUbicacion;
+
+    @Column(name = "posicion",  nullable = false,  length = 45)
+    private String posicion;
+
+    public Ubicaciones(String posicion) {
+        this.posicion = posicion;
+    }
 }
