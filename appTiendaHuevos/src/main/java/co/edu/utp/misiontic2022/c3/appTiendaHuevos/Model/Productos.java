345902ad55f64b5d6705d505c29a140f55cd8ef5
Pedro Gustavo Melendez Rivera <pmelendez@sena.edu.co>
2022-09-14 20:29:54 -0500

    se creo la representacion de la tabla productos de la base de datos

--- appTiendaHuevos/src/main/java/co/edu/utp/misiontic2022/c3/appTiendaHuevos/Model/Productos.java
+++ appTiendaHuevos/src/main/java/co/edu/utp/misiontic2022/c3/appTiendaHuevos/Model/Productos.java
@@ -1,2 +1,28 @@
-package co.edu.utp.misiontic2022.c3.appTiendaHuevos.Model;public class Productos {
+package co.edu.utp.misiontic2022.c3.appTiendaHuevos.Model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "productos")
+public class Productos {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_producto",  nullable = false, unique = true)
+    private Integer idProducto;
+
+    @Column(name = "descripcion",  nullable = false,  length = 45)
+    private String descripcion;
+
+    public Productos(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
 }
