fc514708389dd436f0cde4cfa2207782dfbd3fbe
Pedro Gustavo Melendez Rivera <pmelendez@sena.edu.co>
2022-09-14 20:29:30 -0500

    se creo la representacion de la tabla motivos de la base de datos

--- appTiendaHuevos/src/main/java/co/edu/utp/misiontic2022/c3/appTiendaHuevos/Model/Motivos.java
+++ appTiendaHuevos/src/main/java/co/edu/utp/misiontic2022/c3/appTiendaHuevos/Model/Motivos.java
@@ -1,2 +1,27 @@
-package co.edu.utp.misiontic2022.c3.appTiendaHuevos.Model;public class Motivos {
+package co.edu.utp.misiontic2022.c3.appTiendaHuevos.Model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "motivos")
+public class Motivos {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_motivo",  nullable = false, unique = true)
+    private Integer idMotivo;
+
+    @Column(name = "descripcion",  nullable = false,  length = 45)
+    private String descripcion;
+
+    public Motivos(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
