6e7c2d3fa11c42d03ddaafde6a89dcb6e28e121a
Jonathan Stiven Pulido Ramirez <idiqe2@gmail.com>
2022-09-17 11:52:07 -0500

    Se genera index en el atributo (posicion)

--- appTiendaHuevos/src/main/java/co/edu/utp/misiontic2022/c3/appTiendaHuevos/Model/Ubicaciones.java
+++ appTiendaHuevos/src/main/java/co/edu/utp/misiontic2022/c3/appTiendaHuevos/Model/Ubicaciones.java
@@ -10,14 +10,14 @@ import javax.persistence.*;
 @Getter
 @Setter
 @NoArgsConstructor
-@Table(name = "ubicaciones")
+@Table(name = "ubicaciones", indexes = {@Index(columnList = "posicion")})
 public class Ubicaciones {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_ubicacion",  nullable = false, unique = true)
     private Integer idUbicacion;
 
-    @Column(name = "posicion",  nullable = false,  length = 45)
+    @Column(name = "posicion",  nullable = false,  length = 45, unique = true)
     private String posicion;
 
     public Ubicaciones(String posicion) {
