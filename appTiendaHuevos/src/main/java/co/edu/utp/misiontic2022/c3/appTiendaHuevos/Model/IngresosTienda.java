352661d1bbf028ee2d5826f7c27ba2772f908ab6
Pedro Gustavo Melendez Rivera <pmelendez@sena.edu.co>
2022-09-17 10:28:56 -0500

    Creación de tabla ajustes IngresosTienda

--- appTiendaHuevos/src/main/java/co/edu/utp/misiontic2022/c3/appTiendaHuevos/Model/IngresosTienda.java
+++ appTiendaHuevos/src/main/java/co/edu/utp/misiontic2022/c3/appTiendaHuevos/Model/IngresosTienda.java
@@ -1,2 +1,35 @@
-package co.edu.utp.misiontic2022.c3.appTiendaHuevos.Model;public class IngresosTienda {
+package co.edu.utp.misiontic2022.c3.appTiendaHuevos.Model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "ingresos_tienda")
+
+public class IngresosTienda {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_ingreso_tienda",  nullable = false, unique = true)
+    private Integer idIngresoTienda;
+
+    @Column(name = "fecha",  nullable = false,  length = 15)
+    private String fecha;
+
+    @Column(name = "cantidad",  nullable = false)
+    private Integer cantidad;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name="id_producto")
+    private Productos idProducto;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name="id_ubicacion")
+    private Ubicaciones idUbicacion;
+
 }
