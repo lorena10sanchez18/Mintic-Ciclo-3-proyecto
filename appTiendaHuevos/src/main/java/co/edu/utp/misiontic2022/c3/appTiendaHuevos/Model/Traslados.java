46deebf648a6e36c10c46411456c8067c5d0d457
Pedro Gustavo Melendez Rivera <pmelendez@sena.edu.co>
2022-09-17 10:29:34 -0500

    Creación de tabla ajustes Traslados

--- appTiendaHuevos/src/main/java/co/edu/utp/misiontic2022/c3/appTiendaHuevos/Model/Traslados.java
+++ appTiendaHuevos/src/main/java/co/edu/utp/misiontic2022/c3/appTiendaHuevos/Model/Traslados.java
@@ -1,2 +1,41 @@
-package co.edu.utp.misiontic2022.c3.appTiendaHuevos.Model;public class Traslados {
+package co.edu.utp.misiontic2022.c3.appTiendaHuevos.Model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "traslados")
+
+public class Traslados {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_traslado",  nullable = false, unique = true)
+    private Integer idTraslado;
+
+    @Column(name = "fecha",  nullable = false,  length = 15)
+    private String fecha;
+
+    @Column(name = "cantidad",  nullable = false)
+    private Integer cantidad;
+
+    @Column(name = "observaciones",  nullable = false,  length = 200)
+    private String observaciones;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name="id_producto")
+    private Productos idProducto;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name="id_bodega")
+    private Bodegas origen;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name="id_bodega")
+    private Bodegas destino;
 }
