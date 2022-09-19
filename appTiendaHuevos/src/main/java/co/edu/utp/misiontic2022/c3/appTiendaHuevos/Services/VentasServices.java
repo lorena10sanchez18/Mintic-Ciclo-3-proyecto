c6d375d686353bd25ac2e41417ebfc03a3681e79
Jonathan Stiven Pulido Ramirez <idiqe2@gmail.com>
2022-09-19 17:42:18 -0500

    Se crea la clase services que implementa el Interfaz IVentasGateway e IVentasRepository para realizar la logica de las consultas

--- appTiendaHuevos/src/main/java/co/edu/utp/misiontic2022/c3/appTiendaHuevos/Services/VentasServices.java
+++ appTiendaHuevos/src/main/java/co/edu/utp/misiontic2022/c3/appTiendaHuevos/Services/VentasServices.java
@@ -1,2 +1,30 @@
-package co.edu.utp.misiontic2022.c3.appTiendaHuevos.Services;public class VentasServices {
+package co.edu.utp.misiontic2022.c3.appTiendaHuevos.Services;
+
+import co.edu.utp.misiontic2022.c3.appTiendaHuevos.Model.Ventas;
+import co.edu.utp.misiontic2022.c3.appTiendaHuevos.Repository.IVentasRepository;
+import co.edu.utp.misiontic2022.c3.appTiendaHuevos.Services.Gateway.IVentasGateway;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class VentasServices implements IVentasGateway {
+
+    public final IVentasRepository iVentasRepository;
+
+    public VentasServices(IVentasRepository iVentasRepository) {
+        this.iVentasRepository = iVentasRepository;
+    }
+
+    @Override
+    public List<Ventas> buscarPorRangoFechas(Date fechaInicial, Date fechaFinal) {
+        List<Ventas> listaVentas = iVentasRepository.buscarPorRangoFechas(fechaInicial, fechaFinal);
+        return listaVentas;
+
+    }
+    @Override
+    public Integer calcularTotalVentasPorRanfoFechas(Date fechaInicial, Date fechaFinal) {
+        return iVentasRepository.calcularTotalVentasPorRanfoFechas(fechaInicial,fechaFinal);
+    }
 }
