218cc80f6cb3b1526be429f43b196ea9c9ef2536
Jonathan Stiven Pulido Ramirez <idiqe2@gmail.com>
2022-09-19 17:33:00 -0500

    Se crean las dos consultas que se hacen a la base de datos

--- appTiendaHuevos/src/main/java/co/edu/utp/misiontic2022/c3/appTiendaHuevos/Repository/IVentasRepository.java
+++ appTiendaHuevos/src/main/java/co/edu/utp/misiontic2022/c3/appTiendaHuevos/Repository/IVentasRepository.java
@@ -1,2 +1,20 @@
-package co.edu.utp.misiontic2022.c3.appTiendaHuevos.Repository;public interface IVentasRepository {
+package co.edu.utp.misiontic2022.c3.appTiendaHuevos.Repository;
+
+import co.edu.utp.misiontic2022.c3.appTiendaHuevos.Model.Ventas;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
+
+@Repository
+public interface IVentasRepository extends JpaRepository<Ventas, Integer> {
+
+    @Query(value = "SELECT * FROM ventas WHERE fecha BETWEEN :fechaInicial AND :fechaFinal",nativeQuery = true)
+    List<Ventas> buscarPorRangoFechas(@Param("fechaInicial") Date fechaInicial, @Param("fechaFinal") Date fechaFinal);
+
+    @Query(value = "SELECT SUM(valor_total) FROM ventas WHERE fecha BETWEEN :fechaInicial AND :fechaFinal", nativeQuery = true)
+    Integer calcularTotalVentasPorRanfoFechas(@Param("fechaInicial") Date fechaInicial, @Param("fechaFinal") Date fechaFinal);
 }
