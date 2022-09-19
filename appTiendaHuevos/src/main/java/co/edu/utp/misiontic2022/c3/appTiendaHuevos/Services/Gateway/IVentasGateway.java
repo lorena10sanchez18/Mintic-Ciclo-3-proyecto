336732ca2bb4369e0098006c2ad1804e96cad4a6
Jonathan Stiven Pulido Ramirez <idiqe2@gmail.com>
2022-09-19 17:58:01 -0500

    Se crea la interfaz para separar la logica del services y el repository de ventas

--- appTiendaHuevos/src/main/java/co/edu/utp/misiontic2022/c3/appTiendaHuevos/Services/Gateway/IVentasGateway.java
+++ appTiendaHuevos/src/main/java/co/edu/utp/misiontic2022/c3/appTiendaHuevos/Services/Gateway/IVentasGateway.java
@@ -1,2 +1,13 @@
-package co.edu.utp.misiontic2022.c3.appTiendaHuevos.Services.Gateway;public interface IVentasGateway {
+package co.edu.utp.misiontic2022.c3.appTiendaHuevos.Services.Gateway;
+
+import co.edu.utp.misiontic2022.c3.appTiendaHuevos.Model.Ventas;
+
+import java.util.Date;
+import java.util.List;
+
+public interface IVentasGateway {
+
+    List<Ventas> buscarPorRangoFechas(Date fechaInicial, Date fechaFinal);
+
+    Integer calcularTotalVentasPorRanfoFechas(Date fechaInicial, Date fechaFinal);
 }
