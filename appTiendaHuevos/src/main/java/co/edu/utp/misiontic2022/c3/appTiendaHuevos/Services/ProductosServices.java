95a834ed621ac65687dce30e3d26bd88ceb8a9bc
Pedro Gustavo Melendez Rivera <pmelendez@sena.edu.co>
2022-09-27 11:54:02 -0500

    se agrego buscarProductoPorNombre y actualizarValorProducto

--- appTiendaHuevos/src/main/java/co/edu/utp/misiontic2022/c3/appTiendaHuevos/Services/ProductosServices.java
+++ appTiendaHuevos/src/main/java/co/edu/utp/misiontic2022/c3/appTiendaHuevos/Services/ProductosServices.java
@@ -12,16 +12,28 @@ public class ProductosServices implements IProductosGateway {
     public final IProductosRepository iProductosRepository;
 
     public ProductosServices(IProductosRepository iProductosRepository) {
-        this.iProductosRepository = iProductosRepository;}
+        this.iProductosRepository = iProductosRepository;
+    }
 
     @Override
     public List<Productos> buscarProductos() {
         return iProductosRepository.buscarProductos();
     }
 
-     @Override
-    public void insertarProducto(Productos producto){
+    @Override
+    public void insertarProducto(Productos producto) {
         iProductosRepository.save(producto);
     }
 
+    @Override
+    public Productos buscarProductoPorNombre(String buscarPorNombre) {
+        return iProductosRepository.findByDescripcionProductos(buscarPorNombre);
+    }
+
+    @Override
+    public void actualizarValorProducto(Productos producto) {
+        Productos updateProducto = iProductosRepository.findByDescripcionProductos(producto.getDescripcionProductos());
+        updateProducto.setValorVenta(producto.getValorVenta());
+        iProductosRepository.save(updateProducto);
+    }
 }
