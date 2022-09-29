5b944893846b89edbf2a9aace48d9b6f1a9cd37f
Jonathan Stiven Pulido Ramirez <idiqe2@gmail.com>
2022-09-29 00:28:55 -0500

    Se crea vista de actualizar producto se realiza la logica del primer formulario

--- AppTiendaHuevos/src/app/containers/administrador/actualizar-producto/actualizar-producto.component.ts
+++ AppTiendaHuevos/src/app/containers/administrador/actualizar-producto/actualizar-producto.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ProductosService } from '@core/Services/productos.service';
 
 @Component({
   selector: 'app-actualizar-producto',
   templateUrl: './actualizar-producto.component.html',
-  styleUrls: ['./actualizar-producto.component.css']
+  styleUrls: ['./actualizar-producto.component.css'],
 })
 export class ActualizarProductoComponent implements OnInit {
+  formularioBuscar: FormGroup;
+  formularioActualizar: FormGroup;
+  resMostrarProductos!: any;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private servicioBuscar: ProductosService
+  ) {
+    this.formularioBuscar = this.fb.group({
+      nombreProducto: ['', [Validators.required]],
+    });
 
-  ngOnInit(): void {
+    this.formularioActualizar = this.fb.group({
+      ventaUnitaria: ['', [Validators.required]]
+    }); 
   }
 
+  ngOnInit(): void {
+    this.servicioBuscar.mostrarProducto().subscribe((respuestaback) => {
+      this.resMostrarProductos = respuestaback;
+    });
+  }
+
+  actualizar(){
+
+  }
+  cancelar(){
+    this.formularioBuscar.reset();
+    this.formularioActualizar.reset();
+  }
 }
