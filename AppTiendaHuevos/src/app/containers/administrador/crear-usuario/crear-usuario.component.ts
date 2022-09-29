ac881e0afbbc82e8eecd4fe0a347b8ad7a5e1b48
Pedro Gustavo Melendez Rivera <pmelendez@sena.edu.co>
2022-09-29 09:34:58 -0500

    se actualizo css de crear.usuario.ts

--- AppTiendaHuevos/src/app/containers/administrador/crear-usuario/crear-usuario.component.ts
+++ AppTiendaHuevos/src/app/containers/administrador/crear-usuario/crear-usuario.component.ts
@@ -1,15 +1,59 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UsuariosService } from '@core/Services/usuarios.service';
 
 @Component({
   selector: 'app-crear-usuario',
   templateUrl: './crear-usuario.component.html',
-  styleUrls: ['./crear-usuario.component.css']
+  styleUrls: ['./crear-usuario.component.css'],
 })
 export class CrearUsuarioComponent implements OnInit {
+  formularioCrearUsuario: FormGroup;
+  nombreUsuario!: String;
+  cedula!: Number;
+  contrasenia!: any;
+  rol!: String;
+  estado!: String;
+  usuarios!: {};
+  respuestaback!: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder,
+    private servicesCrearUsuario: UsuariosService
+  ) {
+    this.formularioCrearUsuario = this.fb.group({
+      nombreUsuario: ['', [Validators.required]],
+      cedula: ['', [Validators.required]],
+      contrasenia: ['', [Validators.required]],
+      rol:[''],
+      estado:[''],
+    });
   }
 
+  ngOnInit(): void {}
+
+  crear() {
+    this.cedula = this.formularioCrearUsuario.value.cedula;
+    this.contrasenia = this.formularioCrearUsuario.value.contrasenia;
+    this.rol = this.formularioCrearUsuario.value.rol;
+    this.estado = this.formularioCrearUsuario.value.estado;
+    this.nombreUsuario = this.formularioCrearUsuario.value.nombreUsuario;
+
+    this.usuarios = {
+      cedula: this.cedula,
+      contrasenia: this.contrasenia,
+      rol: this.rol,
+      estado: this.estado,
+      nombre: this.nombreUsuario,
+    };
+    this.servicesCrearUsuario
+      .crearUsuario(this.usuarios)
+      .subscribe((response: any) => {
+        this.respuestaback = response.body;
+      });
+  }
+
+  cancelar() {
+    this.formularioCrearUsuario.reset();
+  }
 }
