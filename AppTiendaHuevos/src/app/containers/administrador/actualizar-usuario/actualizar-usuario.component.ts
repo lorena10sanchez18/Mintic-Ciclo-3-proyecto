096a74e5ca7dd6a78b017185a4748b35eca39769
Jonathan Stiven Pulido Ramirez <idiqe2@gmail.com>
2022-10-03 22:05:02 -0500

    Se valida y corrige el pop up de la vista actualizar usuario

--- AppTiendaHuevos/src/app/containers/administrador/actualizar-usuario/actualizar-usuario.component.ts
+++ AppTiendaHuevos/src/app/containers/administrador/actualizar-usuario/actualizar-usuario.component.ts
@@ -5,14 +5,12 @@ import { UsuariosService } from '@core/Services/usuarios.service';
 @Component({
   selector: 'app-actualizar-usuario',
   templateUrl: './actualizar-usuario.component.html',
-  styleUrls: ['./actualizar-usuario.component.css']
+  styleUrls: ['./actualizar-usuario.component.css'],
 })
-
 export class ActualizarUsuarioComponent implements OnInit {
-
   formularioBuscarUsuario: FormGroup;
   formularioActualizarUsuario: FormGroup;
-  resMostrarUsuario !: any;
+  resMostrarUsuario!: any;
   nombreUsuario!: String;
   cedula!: Number;
   contrasenia!: any;
@@ -27,73 +25,60 @@ export class ActualizarUsuarioComponent implements OnInit {
     private fb: FormBuilder,
     private servicesActualizarUsuario: UsuariosService
   ) {
-
     this.formularioBuscarUsuario = this.fb.group({
-        cedula: [''],
+      cedula: ['', [Validators.required]],
     });
 
     this.formularioActualizarUsuario = this.fb.group({
-      nombreUsuario:[''],
-      cedula:[''],
-      contrasenia:[''],
-      rol:[''],
-      estado:[''],
+      nombreUsuario: ['', [Validators.required]],
+      cedula: ['', [Validators.required]],
+      contrasenia: ['', [Validators.required]],
+      rol: ['', [Validators.required]],
+      estado: ['', [Validators.required]],
     });
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
 
+  buscar() {
+    this.cedula = this.formularioBuscarUsuario.value.cedula;
+    this.cedulaJson = {
+      cedula: this.cedula,
+    };
+    this.servicesActualizarUsuario
+      .buscarUsuario(this.cedulaJson)
+      .subscribe((response: any) => {
+        this.resActualizarUsuario = response;
+
+        this.formularioActualizarUsuario.patchValue({
+          nombreUsuario: response.nombre,
+          cedula: response.cedula,
+          contrasenia: response.contrasenia,
+          rol: response.rol,
+          estado: response.estado,
+        });
+      });
   }
 
-
-  
   actualizar() {
-    
     this.cedula = this.formularioActualizarUsuario.value.cedula;
     this.nombreUsuario = this.formularioActualizarUsuario.value.nombreUsuario;
     this.contrasenia = this.formularioActualizarUsuario.value.contrasenia;
     this.rol = this.formularioActualizarUsuario.value.rol;
     this.estado = this.formularioActualizarUsuario.value.estado;
     this.usuarios = {
-    cedula: this.cedula,
-    contrasenia: this.contrasenia,
-    rol: this.rol,
-    estado: this.estado,
-    nombre: this.nombreUsuario
+      cedula: this.cedula,
+      contrasenia: this.contrasenia,
+      rol: this.rol,
+      estado: this.estado,
+      nombre: this.nombreUsuario,
     };
-    console.log(this.usuarios);
+
     this.servicesActualizarUsuario
       .actualizarUsuario(this.usuarios)
       .subscribe((response) => {
         this.resActualizarUsuario = response.body;
-  
       });
-      setTimeout(function(){
-        console.log("waited seconds");
-            }, 2000);
-          
-  }
-
-  buscar(){
-    this.cedula = this.formularioBuscarUsuario.value.cedula;
-    this.cedulaJson = {
-      "cedula": this.cedula     
-    };
-    this.servicesActualizarUsuario.buscarUsuario(this.cedulaJson).subscribe((response:any)=>{
-      this.resActualizarUsuario = response;
-      console.log(response);
-     
-    this.formularioActualizarUsuario.patchValue({
-      nombreUsuario:response.nombre,
-      cedula: response.contrasenia,
-      rol: response.rol,
-      estado: response.estado
-    });
-          
-    });
- 
-   
-  
   }
 
   cancelar() {
